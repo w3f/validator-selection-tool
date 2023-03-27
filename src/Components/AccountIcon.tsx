05c87b3dcc4a5be68f3e6ba2c10b13d84860ad09
dnlklmn <pelicansinberlin@gmail.com>
2023-03-27 13:56:02 +0200

    identicon + address

---
+++ src/Components/AccountIcon.tsx
@@ -0,0 +1,20 @@
+import Identicon from "@polkadot/react-identicon"
+import { stringShorten } from "@polkadot/util"
+
+export function Accounticon({
+  address,
+  name,
+}: {
+  address: string
+  name?: string
+  size?: number
+  textClassName?: string
+}) {
+  const btnTitle = name || address
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <Identicon value={address} theme="polkadot" size={40} />
+      <div className="text-h5 font-unbounded">{stringShorten(btnTitle, 4)}</div>
+    </div>
+  )
+}
