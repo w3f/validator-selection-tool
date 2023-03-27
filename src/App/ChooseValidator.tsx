beada4091516a2f29a6c246a45f16f5ae14abf5c
dnlklmn <pelicansinberlin@gmail.com>
2023-03-27 17:34:10 +0200

    hero fix

--- src/App/ChooseValidator.tsx
+++ src/App/ChooseValidator.tsx
@@ -115,7 +115,7 @@ export const ChooseValidator: React.FC<{
         <div className="h-6 w-[2px] bg-gray-300" />
         <span className="text-xl font-[Unbounded]">Validator Picker</span>
       </div>
-      <div className="flex flex-col items-center mx-auto gap-4 w-1/2 my-12 text-center">
+      <div className="flex flex-col items-start gap-4 w-1/2 mt-0 mb-4">
         <span className="text-h3 font-unbounded">
           Refine your validator set
         </span>
@@ -127,7 +127,6 @@ export const ChooseValidator: React.FC<{
           validators and we are going to provide you with a list of suggested
           addresses based on your personal preferences.
         </span>
-        <Button width="fit">Start now!</Button>
       </div>
       <div className="flex gap-16">
         <div className="w-full bg-white shadow-lg rounded-lg overflow-clip flex h-fit">
