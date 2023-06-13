e0b9e1c70f0f37f86acf75c4d0765d7bbe03868e
dnlklmn <pelicansinberlin@gmail.com>
2023-06-13 20:20:50 +0200

    reorg

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,50 @@
-import { ChooseValidator } from "./ChooseValidator"
+import { useStateObservable, withDefault } from "@react-rxjs/core"
+import { ResultsState, resultsState$, isToughCookie$ } from "@/state"
+import Picker from "./Picker"
+import { map } from "rxjs"
+import Header from "./Header"
+import Hero from "./Hero"
+import { Results } from "./Results"
+
+const isInit$ = resultsState$.pipeState(
+  map((x) => x === ResultsState.INIT),
+  withDefault(true),
+)
+
+const isDone$ = resultsState$.pipeState(
+  map((x) => x === ResultsState.PERFECT),
+  withDefault(false),
+)
+
+const isPerfect$ = resultsState$.pipeState(
+  map((x) => x === ResultsState.PERFECT),
+  withDefault(false),
+)
 
 export const App = () => {
+  const isDone = useStateObservable(isDone$)
+  const isInit = useStateObservable(isInit$)
+  const isToughCookie = useStateObservable(isToughCookie$)
+  const isPerfect = useStateObservable(isPerfect$)
   return (
     <>
       <style>
         @import
         url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Unbounded:wght@500&display=swap');
       </style>
-      <ChooseValidator />
+      <div className="flex flex-col gap-5 pb-24 px-16">
+        <Header />
+        {isToughCookie && !isPerfect ? (
+          <div>WOW! You are a tough MF!</div>
+        ) : null}
+
+        <div
+          className={`flex ${isInit ? "items-center" : "items-start"} gap-16`}
+        >
+          {isDone ? null : <Picker />}
+          {isInit ? <Hero /> : <Results />}
+        </div>
+      </div>
     </>
   )
 }
--- src/App/Results.tsx
+++ src/App/Results.tsx
@@ -1,9 +1,15 @@
-import { results$ } from "@/state"
-import { Subscribe, SUSPENSE } from "@react-rxjs/core"
+import { results$, onReset, resultsState$, ResultsState } from "@/state"
+import {
+  Subscribe,
+  SUSPENSE,
+  useStateObservable,
+  withDefault,
+} from "@react-rxjs/core"
 import { map } from "rxjs"
-import { Loading } from "./Loading"
-import { AccountIcon } from "@/Components/AccountIcon"
-import { Field, sections } from "./ChooseValidator"
+import { Loading } from "../Components/Loading"
+import { AccountIcon } from "../Components/AccountIcon"
+import { Field, sections } from "./Picker"
+import Button from "../Components/Button"
 
 const jsxResults$ = results$.pipeState(
   map((validators) =>
@@ -35,7 +41,78 @@ const jsxResults$ = results$.pipeState(
     ),
   ),
 )
+const isInit$ = resultsState$.pipeState(
+  map((x) => x === ResultsState.INIT),
+  withDefault(true),
+)
+
+const isInsufficient$ = resultsState$.pipeState(
+  map((x) => x === ResultsState.INSUFICIENT),
+  withDefault(false),
+)
+
+const isGoodEnough$ = resultsState$.pipeState(
+  map((x) => x === ResultsState.GOOD_ENOUGH),
+  withDefault(false),
+)
+
+const isPerfect$ = resultsState$.pipeState(
+  map((x) => x === ResultsState.PERFECT),
+  withDefault(false),
+)
+
+const Reset: React.FC = () => {
+  const isInit = useStateObservable(isInit$)
+  return isInit ? null : (
+    <Button secondary onClick={() => onReset()} width="fit">
+      Reset
+    </Button>
+  )
+}
 
 export const Results: React.FC = () => {
-  return <Subscribe fallback={<Loading size={30} />}>{jsxResults$}</Subscribe>
+  const isGoodEnough = useStateObservable(isGoodEnough$)
+  const isPerfect = useStateObservable(isPerfect$)
+  const isInsufficient = useStateObservable(isInsufficient$)
+
+  return (
+    <div className="w-full flex flex-col gap-3">
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-2">
+          <div className="flex h-6 items-center justify-between">
+            <span className="text-h5 font-unbounded">Results</span>
+            <Reset />
+          </div>
+        </div>
+        <div className="flex gap-2 items-center text-body-2">
+          <span className="font-inter">Confidence:</span>
+          {isInsufficient && (
+            <div className="flex items-center gap-1.5">
+              <div className="w-4 h-4 rounded-full  bg-orange-400" />
+              <span className="font-semibold font-inter text-orange-400">
+                Low
+              </span>
+            </div>
+          )}
+          {isGoodEnough && (
+            <div className="flex items-center gap-1.5">
+              <div className="w-4 h-4 rounded-full  bg-green-500" />
+              <span className="text-green-500 font-semibold font-inter">
+                Good enough
+              </span>
+            </div>
+          )}
+          {isPerfect && (
+            <div className="flex items-center gap-1.5">
+              <div className="w-4 h-4 rounded-full  bg-green-600" />
+              <span className="text-green-600 font-semibold font-inter">
+                Perfect
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
+      <Subscribe fallback={<Loading size={16} />}>{jsxResults$}</Subscribe>
+    </div>
+  )
 }
