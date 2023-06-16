5ddc0a62006c87b06d3327ce04a7ee3e6fd2c1b1
Jonas <jonas@web3.foundation>
2023-06-16 10:40:00 +0200

    streamlined the info text

--- src/App/Hero.tsx
+++ src/App/Hero.tsx
@@ -2,16 +2,22 @@ export default function Hero() {
   return (
     <div className="w-full h-full mb-4">
       <div className="h-full flex flex-col items-start justify-center gap-4 pr-24 pl-6 mb-4">
-        <span className="text-h3 font-unbounded">
-          Refine your validator set
-        </span>
+        <span className="text-h3 font-unbounded">Tailor Your Nominations</span>
         <span className="text-body-2">
-          This tool is designed to help you choose the Validators that are
-          better suited for your preferences.
-          <div className="mb-3" />
-          We are going to ask you to decide between a few (5-8) pairs of
-          validators and we are going to provide you with a list of suggested
-          addresses based on your personal preferences.
+          This tool is engineered to streamline your nominations based on your
+          preferences, using the most up-to-date validator data.
+          <br />
+          <br />
+          Your role involves making decisions between pairs of validators
+          (typically 5-8), enabling the algorithm to learn from your choices.
+          <br />
+          <br />
+          Upon conclusion, you'll be provided a list of top-ranking validators
+          matching your preferences. You'll have the flexibility to adjust these
+          recommendations before exporting as per your requirement.
+          <br />
+          <br />
+          To start, make your first selection between the validators presented.
         </span>
       </div>
     </div>
