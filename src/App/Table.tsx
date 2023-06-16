7340c5c0736d75c3db940ad5418587ee4f7d07f0
Josep M Sobrepere <jm.sobrepere@gmail.com>
2023-06-16 07:55:12 +0200

    a few tweaks on `Results`

--- src/App/Table.tsx
+++ src/App/Table.tsx
@@ -1,132 +1,136 @@
 import { AccountIcon } from "@/Components/AccountIcon"
-import CheckBox from "@/Components/CheckBox"
 import { results$, resultsState$, ResultsState } from "@/state"
-import { SUSPENSE, withDefault } from "@react-rxjs/core"
+import { SUSPENSE, useStateObservable, withDefault } from "@react-rxjs/core"
 import { map } from "rxjs"
 import { sections, Field } from "./Picker"
+import { useState } from "react"
 
 const isPerfect$ = resultsState$.pipeState(
   map((x) => x === ResultsState.PERFECT),
   withDefault(false),
 )
 
-export default function Table({ items = 16 }) {
+export default function Table() {
+  const isPerfect = useStateObservable(isPerfect$)
+  const [items, setItems] = useState(16)
   return results$.pipeState(
     map((validators) =>
       Array.isArray(validators) ? (
-        <div className="w-full whitespace-nowrap text-body-2 leading-5 flex ">
-          <div className="w-fit flex flex-col gap-2">
-            <span className="sticky top-0 text-caption bg-bg-default border-b-[1px] pb-1 ">
-              <div className="w-5 h-5 p-[2px] mr-3">
-                <input
-                  onChange={(e) => {
-                    let checkboxes = document.getElementsByName(
-                      "selectedAddress",
-                    ) as unknown as HTMLInputElement[]
-
-                    let allChecked: boolean
-
-                    for (let i = 0; i < checkboxes.length; i++) {
-                      checkboxes[i].checked === true
-                        ? (allChecked = true)
-                        : (allChecked = false)
-                    }
-
-                    checkboxes.forEach((checkbox) => {
-                      allChecked && e.currentTarget.checked === false
-                        ? (checkbox.checked = false)
-                        : (checkbox.checked = true)
-                    })
-                  }}
-                  className="w-full h-full"
-                  type="checkbox"
-                  name="mainCheckbox"
-                />
-              </div>
-            </span>
-            {validators.slice(0, items).map((validator) => (
-              <div
-                key={validator.address}
-                className=" flex items-center pb-4 pt-3 border-b-[1px]"
-              >
+        <>
+          <div className="w-full whitespace-nowrap text-body-2 leading-5 flex ">
+            <div className="w-fit flex flex-col gap-2">
+              <span className="sticky top-0 text-caption bg-bg-default border-b-[1px] pb-1 ">
                 <div className="w-5 h-5 p-[2px] mr-3">
                   <input
-                    onChange={(e) => {
-                      const mainCheckbox = document.getElementsByName(
-                        "mainCheckbox",
+                    onChange={() => {
+                      const checkboxes = document.getElementsByName(
+                        "selectedAddress",
                       ) as unknown as HTMLInputElement[]
-                      e.currentTarget.checked === false &&
-                        (mainCheckbox[0].checked = false)
+
+                      const allChecked = [...checkboxes].every(
+                        (checkbox) => checkbox.checked,
+                      )
+
+                      checkboxes.forEach((checkbox) => {
+                        checkbox.checked = !allChecked
+                      })
                     }}
                     className="w-full h-full"
                     type="checkbox"
-                    value={validator.address}
-                    name="selectedAddress"
+                    name="mainCheckbox"
                   />
                 </div>
-              </div>
-            ))}
-          </div>
-          <div className="w-full flex flex-col gap-2">
-            <span className="sticky top-0 text-caption text-gray-400 bg-bg-default border-b-[1px] pb-1">
-              Account
-            </span>
-            {validators.slice(0, items).map((validator) => (
-              <div
-                key={validator.address}
-                className="w-full flex items-center pb-4 pt-3 pr-8 border-b-[1px]"
-              >
-                <AccountIcon
-                  small
-                  showAddress={isPerfect$.getValue()}
-                  address={validator.address}
-                />
-              </div>
-            ))}
-          </div>
-          <div className="w-full flex flex-col gap-2 ">
-            <span className="sticky top-0 text-caption text-gray-400 bg-bg-default pr-4 border-b-[1px] pb-1 ">
-              Score
-            </span>
-            {validators.slice(0, items).map((validator) => (
-              <div
-                key={validator.address}
-                className="w-full flex items-center pb-4 pt-3 border-b-[1px] pr-4"
-              >
-                {validator.score.toFixed(2)}
-              </div>
-            ))}
-          </div>
-          {Object.entries(sections).map(([key, title], index) => (
-            <div className="w-full flex flex-col gap-2">
-              <span
-                className={`sticky top-0 text-caption text-gray-400 bg-bg-default border-b-[1px] pb-1  ${
-                  index === Object.values(sections).length - 1
-                    ? "text-right w-full"
-                    : "pr-4"
-                }`}
-              >
-                {title}
               </span>
               {validators.slice(0, items).map((validator) => (
                 <div
                   key={validator.address}
-                  className="w-full flex items-center pb-4 pt-3 border-b-[1px]"
+                  className=" flex items-center pb-4 pt-3 border-b-[1px]"
                 >
-                  <Field
-                    className={
-                      index === Object.values(sections).length - 1
-                        ? "text-right w-full"
-                        : "pr-4"
-                    }
-                    validator={validator}
-                    field={key as any}
+                  <div className="w-5 h-5 p-[2px] mr-3">
+                    <input
+                      onChange={(e) => {
+                        const mainCheckbox = document.getElementsByName(
+                          "mainCheckbox",
+                        ) as unknown as HTMLInputElement[]
+                        e.currentTarget.checked === false &&
+                          (mainCheckbox[0].checked = false)
+                      }}
+                      className="w-full h-full"
+                      type="checkbox"
+                      value={validator.address}
+                      name="selectedAddress"
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="w-full flex flex-col gap-2">
+              <span className="sticky top-0 text-caption text-gray-400 bg-bg-default border-b-[1px] pb-1">
+                Account
+              </span>
+              {validators.slice(0, items).map((validator) => (
+                <div
+                  key={validator.address}
+                  className="w-full flex items-center pb-4 pt-3 pr-8 border-b-[1px]"
+                >
+                  <AccountIcon
+                    small
+                    showAddress={isPerfect}
+                    address={validator.address}
                   />
                 </div>
               ))}
             </div>
-          ))}
-        </div>
+            <div className="w-full flex flex-col gap-2 ">
+              <span className="sticky top-0 text-caption text-gray-400 bg-bg-default pr-4 border-b-[1px] pb-1 ">
+                Score
+              </span>
+              {validators.slice(0, items).map((validator) => (
+                <div
+                  key={validator.address}
+                  className="w-full flex items-center pb-4 pt-3 border-b-[1px] pr-4"
+                >
+                  {validator.score.toFixed(2)}
+                </div>
+              ))}
+            </div>
+            {Object.entries(sections).map(([key, title], index) => (
+              <div className="w-full flex flex-col gap-2">
+                <span
+                  className={`sticky top-0 text-caption text-gray-400 bg-bg-default border-b-[1px] pb-1  ${
+                    index === Object.values(sections).length - 1
+                      ? "text-right w-full"
+                      : "pr-4"
+                  }`}
+                >
+                  {title}
+                </span>
+                {validators.slice(0, items).map((validator) => (
+                  <div
+                    key={validator.address}
+                    className="w-full flex items-center pb-4 pt-3 border-b-[1px]"
+                  >
+                    <Field
+                      className={
+                        index === Object.values(sections).length - 1
+                          ? "text-right w-full"
+                          : "pr-4"
+                      }
+                      validator={validator}
+                      field={key as any}
+                    />
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+          <button
+            className="text-body-2"
+            onClick={() => setItems((prev) => prev + 16)}
+          >
+            Load 16 more
+          </button>
+        </>
       ) : validators === SUSPENSE ? (
         validators
       ) : (
